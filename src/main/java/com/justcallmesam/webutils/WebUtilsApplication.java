24e53e7198c68282eb1fdd9fcfa8db8d9c51a91a
sam <sam@justcallmesam.com>
2024-09-14 00:29:05 -0500

    init app

---
+++ src/main/java/com/justcallmesam/webutils/WebUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.justcallmesam.webutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebUtilsApplication.class, args);
+	}
+
+}
